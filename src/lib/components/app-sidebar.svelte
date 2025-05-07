f9f7a80dea3d5c53327c32b63fe379bd5628fb60
Kurund Jalmi <kurund.jalmi@gmail.com>
2025-05-07 23:41:42 +0530

    fix formatting

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,60 +1,61 @@
 <script lang="ts">
-import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-import LayoutGrid  from '@lucide/svelte/icons/layout-grid';
-import FileQuestion  from '@lucide/svelte/icons/file-question';
-import ClipboardList  from '@lucide/svelte/icons/clipboard-list';
-import User  from '@lucide/svelte/icons/user';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import LayoutGrid from '@lucide/svelte/icons/layout-grid';
+	import FileQuestion from '@lucide/svelte/icons/file-question';
+	import ClipboardList from '@lucide/svelte/icons/clipboard-list';
+	import User from '@lucide/svelte/icons/user';
 
-// Menu items.
+	// Menu items.
 
-     // Menu items.
-  const items = [
-    {
-      title: "Dashboard",
-      url: "/dashboard",
-      icon: LayoutGrid,
-    },
-    {
-      title: "Question Bank",
-    url: "/questionbank",
-      icon: FileQuestion,
-    },
-    {
-      title: "Tests",
-      url: "/tests",
-      icon: ClipboardList,
-    },
-    {
-      title: "User Management",
-      url: "/user",
-      icon: User,
-    },
-
-  ];
+	// Menu items.
+	const items = [
+		{
+			title: 'Dashboard',
+			url: '/dashboard',
+			icon: LayoutGrid
+		},
+		{
+			title: 'Question Bank',
+			url: '/questionbank',
+			icon: FileQuestion
+		},
+		{
+			title: 'Tests',
+			url: '/tests',
+			icon: ClipboardList
+		},
+		{
+			title: 'User Management',
+			url: '/user',
+			icon: User
+		}
+	];
 </script>
-    
+
 <Sidebar.Root>
-    <Sidebar.Content>
-      <Sidebar.Group>
-        <Sidebar.GroupLabel><h4 class="scroll-m-20 text-xl font-extrabold tracking-tighter text-black pb-4 w-ful">
-            Sashakt
-          </h4></Sidebar.GroupLabel>
-        <Sidebar.GroupContent>
-          <Sidebar.Menu>
-            {#each items as item (item.title)}
-              <Sidebar.MenuItem>
-                <Sidebar.MenuButton>
-                  {#snippet child({ props })}
-                    <a href={item.url} {...props}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  {/snippet}
-                </Sidebar.MenuButton>
-              </Sidebar.MenuItem>
-            {/each}
-          </Sidebar.Menu>
-        </Sidebar.GroupContent>
-      </Sidebar.Group>
-    </Sidebar.Content>
-  </Sidebar.Root>
+	<Sidebar.Content>
+		<Sidebar.Group>
+			<Sidebar.GroupLabel
+				><h4 class="w-ful scroll-m-20 pb-4 text-xl font-extrabold tracking-tighter text-black">
+					Sashakt
+				</h4></Sidebar.GroupLabel
+			>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
+					{#each items as item (item.title)}
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton>
+								{#snippet child({ props })}
+									<a href={item.url} {...props}>
+										<item.icon />
+										<span>{item.title}</span>
+									</a>
+								{/snippet}
+							</Sidebar.MenuButton>
+						</Sidebar.MenuItem>
+					{/each}
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
+	</Sidebar.Content>
+</Sidebar.Root>
--- src/routes/(admin)/dashboard/+page.svelte
+++ src/routes/(admin)/dashboard/+page.svelte
@@ -1,81 +1,90 @@
 <script lang="ts">
-	import Button from "$lib/components/ui/button/button.svelte";
-    import * as Dialog from "$lib/components/ui/dialog/index.js";
-    import Label from "$lib/components/ui/label/label.svelte";
-    import Info from "@lucide/svelte/icons/info";
-    const information = [
-    { title: "Manage Question Banks", description: "Effortlessly create, edit, and organize question banks to keep everything at your fingertips."},
-    { title: "Generate Test Templates", description: "Customize test templates to suit diverse requirements, ensuring consistency and ease of use." },
-    { title: "Create and Assign Tests", description: "Build, schedule, and assign tests seamlessly to streamline the evaluation process." },
-    { title: "Monitor Analytics", description: "Gain valuable insights through detailed analytics to track performance and progress." },
-    { title: "User Management", description: "Add, manage, and oversee user roles to ensure collaboration and control." }
-    ]
-    
-    const stats_box=[
-        {title:'Total Questions', description: "Stats and Generated Insights"},
-        {title:'Tests', description: "Stats and Generated Insights"},
-        {title:'Users', description: "Stats and Generated Insights"},
-    ]
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import Label from '$lib/components/ui/label/label.svelte';
+	import Info from '@lucide/svelte/icons/info';
+	const information = [
+		{
+			title: 'Manage Question Banks',
+			description:
+				'Effortlessly create, edit, and organize question banks to keep everything at your fingertips.'
+		},
+		{
+			title: 'Generate Test Templates',
+			description:
+				'Customize test templates to suit diverse requirements, ensuring consistency and ease of use.'
+		},
+		{
+			title: 'Create and Assign Tests',
+			description:
+				'Build, schedule, and assign tests seamlessly to streamline the evaluation process.'
+		},
+		{
+			title: 'Monitor Analytics',
+			description:
+				'Gain valuable insights through detailed analytics to track performance and progress.'
+		},
+		{
+			title: 'User Management',
+			description: 'Add, manage, and oversee user roles to ensure collaboration and control.'
+		}
+	];
 
+	const stats_box = [
+		{ title: 'Total Questions', description: 'Stats and Generated Insights' },
+		{ title: 'Tests', description: 'Stats and Generated Insights' },
+		{ title: 'Users', description: 'Stats and Generated Insights' }
+	];
+</script>
 
+<Dialog.Root open={true}>
+	<Dialog.Content>
+		<Dialog.Header>
+			<Dialog.Title>Welcome to Sashakt!</Dialog.Title>
+			<Dialog.Description>Here’s what all you can do</Dialog.Description>
+		</Dialog.Header>
+		<div class="w-full rounded-xl p-4" style="background:#F0F9FF">
+			{#each information as item}
+				<div class="mb-4">
+					<p class="font-semibold">{item.title}</p>
+					<p class="text-sm font-light">{item.description}</p>
+				</div>
+			{/each}
+		</div>
+		<Button style="background:#0369A1" class="cursor-pointer">Get Started</Button>
+	</Dialog.Content>
+</Dialog.Root>
 
-  </script>
+<div class="mt-10 ml-10 flex w-full items-center align-middle">
+	<span class="flex flex-row">
+		<h2
+			class="mr-2 w-fit scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+		>
+			Dashboard
+		</h2>
+		<Info class="my-auto w-4 align-middle text-xs text-gray-600" />
+	</span>
+</div>
+<Label class="my-auto ml-10 align-middle text-sm font-extralight"
+	>View Data related to Tests and Questions
+</Label>
 
-
-
-  <Dialog.Root open={true} >
-    <Dialog.Content>
-      <Dialog.Header>
-        <Dialog.Title>Welcome to Sashakt!</Dialog.Title>
-        <Dialog.Description>
-            Here’s what all you can do
-        </Dialog.Description>
-      </Dialog.Header>
-      <div class='w-full  p-4 rounded-xl' style="background:#F0F9FF">
-        {#each information as item}
-        <div class="mb-4">
-            <p class="font-semibold">{item.title}</p>
-            <p class="text-sm font-light">{item.description}</p>
-        </div>
-        {/each}
-    </div>
-    <Button style='background:#0369A1' class='cursor-pointer'>Get Started</Button>
-    </Dialog.Content>
-  </Dialog.Root>
-
-
-
-<div class='mt-10 ml-10 w-full  flex align-middle items-center'>
-    <span class="flex flex-row">
-    <h2
-     class="scroll-m-20  pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 w-fit mr-2"
-    >
-    Dashboard
-    </h2>
-    <Info class='text-xs w-4 text-gray-600 align-middle my-auto'/>
-    </span>
-    </div>
-    <Label class="ml-10 text-sm font-extralight my-auto align-middle">View Data related to Tests and Questions </Label>
-
-    <div>
-        <div class='ml-10 mt-4 flex flex-row justify-between  w-3/4'>
-            
-            {#each stats_box as stat}
-                <div class='bg-white w-1/3 m-4 p-4 rounded-xl'>
-                    <p class='font-semibold'>{stat.title}</p>
-                    <p class='text-sm'>{stat.description}</p>
-                    <div class="w-24 h-24"></div>
-                </div>
-            {/each}
-
-        </div>
-        <div class='ml-10 mt-4 flex flex-row justify-between  w-3/4'>
-            <div class='bg-white w-1/2 h-1/2 m-4 mb-10 p-4 rounded-xl'>
-                <div class="w-full h-screen"></div>
-            </div>        
-            <div class='bg-white w-1/2 h-1/2 m-4 p-4 rounded-xl'>
-                <div class="w-full h-screen"></div>
-            </div>
-        </div>
-
-    </div>
+<div>
+	<div class="mt-4 ml-10 flex w-3/4 flex-row justify-between">
+		{#each stats_box as stat}
+			<div class="m-4 w-1/3 rounded-xl bg-white p-4">
+				<p class="font-semibold">{stat.title}</p>
+				<p class="text-sm">{stat.description}</p>
+				<div class="h-24 w-24"></div>
+			</div>
+		{/each}
+	</div>
+	<div class="mt-4 ml-10 flex w-3/4 flex-row justify-between">
+		<div class="m-4 mb-10 h-1/2 w-1/2 rounded-xl bg-white p-4">
+			<div class="h-screen w-full"></div>
+		</div>
+		<div class="m-4 h-1/2 w-1/2 rounded-xl bg-white p-4">
+			<div class="h-screen w-full"></div>
+		</div>
+	</div>
+</div>

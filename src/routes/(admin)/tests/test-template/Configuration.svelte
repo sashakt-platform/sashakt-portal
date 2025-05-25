af3b8ef6186c2ea3f31959ad460d662350d2c5d2
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-05-25 07:30:50 +0530

    Configuration Page Design

--- src/routes/(admin)/tests/test-template/Configuration.svelte
+++ src/routes/(admin)/tests/test-template/Configuration.svelte
@@ -3,92 +3,91 @@
 	import Info from '@lucide/svelte/icons/info';
 	import CircleHelp from '@lucide/svelte/icons/circle-help';
 	import CircleCheck from '@lucide/svelte/icons/circle-check';
-
-	const configSettings = [
-		{
-			icon: Info,
-			title: 'Instructions'
-		},
-		{
-			icon: CircleHelp,
-			title: 'Question settings'
-		},
-		{
-			icon: CircleCheck,
-			title: 'Answer settings'
-		}
-	];
-
-	let settingsState = $state({
-		Instructions: false,
-		QuestionSettings: false,
-		AnswerSettings: false
-	});
+	import Timer from '@lucide/svelte/icons/timer';
+	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
+	import ConfigureBox from './ConfigureBox.svelte';
+	import Clock4 from '@lucide/svelte/icons/clock-4';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
+	import Label from '$lib/components/ui/label/label.svelte';
 </script>
 
 <div class="mx-auto flex h-dvh">
 	<div class="mx-auto mt-10 flex w-1/2 flex-col">
-		{#snippet configurationBox(Icon: any, title: string, visibility: boolean = false)}
-			<div
-				class="hover: hover:bg-secondary my-2 flex w-full cursor-pointer flex-row rounded-lg bg-white px-4 py-5 shadow"
-			>
-				<span class="bg-primary-foreground my-auto w-1/12 rounded-sm p-3">
-					<Icon class={['text-primary mx-auto', visibility && 'rotate-180']} />
-				</span>
-				<p class="w-10/12 p-2 text-xl font-medium">{title}</p>
-				<span class="m-auto w-1/12"><ChevronDown class="mx-auto " /></span>
+		{#snippet headingSubheading(heading: string, subheading: string)}
+			<p class="font-bold">{heading}</p>
+			<p class="text-sm font-extralight">
+				{subheading}
+			</p>
+		{/snippet}
+
+		{#snippet textWithArea(heading: string, subheading: string, lowerBorder: boolean = true)}
+			<div class={['flex flex-row py-8', lowerBorder && 'border-b-1']}>
+				<div class="w-2/5">
+					{@render headingSubheading(heading, subheading)}
+				</div>
+				<div class=" w-3/5">
+					<Textarea placeholder="Enter your instructions here..." />
+				</div>
 			</div>
 		{/snippet}
 
-		<div>
-			<button
-				type="button"
-				class={[
-					'hover: hover:bg-secondary mt-2 flex w-full cursor-pointer flex-row  px-4 py-5 shadow',
-					settingsState.Instructions ? 'bg-secondary rounded-t-2xl' : 'rounded-2xl  bg-white'
-				]}
-				onclick={() => (settingsState.Instructions = !settingsState.Instructions)}
-			>
-				<span class="bg-primary-foreground my-auto w-1/12 rounded-sm p-3">
-					<Info class={['text-primary mx-auto']} />
-				</span>
-				<p class="w-10/12 p-2 text-left text-xl font-medium">Instructions</p>
-				<span class="m-auto w-1/12"
-					><ChevronDown class={['mx-auto ', settingsState.Instructions && 'rotate-180']} /></span
-				>
-			</button>
-			<div
-				class={[
-					!settingsState.Instructions && 'hidden',
-					'flex flex-col rounded-b-2xl bg-white p-4 pl-8 '
-				]}
-			>
-				<div class="flex flex-row border-b-1">
-					<div>
-						<p>Pre-test guidelines</p>
-						<p>Detailed instructions before attempting the test</p>
-					</div>
-					<div></div>
+		<ConfigureBox title="Instructions" Icon={Info}>
+			{@render textWithArea(
+				'Pre-test guidelines',
+				'Detailed instructions before attempting the test'
+			)}
+			{@render textWithArea(
+				'Custom message (opt.)',
+				'Message content after the test completion',
+				false
+			)}
+		</ConfigureBox>
+
+		<ConfigureBox title="Timer Settings" Icon={Timer}>
+			<div class="flex flex-row">
+				<div class="my-auto w-2/5 align-middle">
+					{@render headingSubheading('Start & end time', 'Set the start and end time of the test')}
 				</div>
-				<div>B</div>
+				<div class=" flex w-3/5 flex-row gap-4">
+					<div class="flex w-1/2 flex-col gap-2">
+						<Label for="dateStart" class="my-auto font-extralight">Start Time</Label>
+						<Input type="datetime-local" id="dateStart" />
+					</div>
+					<div class="flex w-1/2 flex-col gap-2">
+						<Label for="dateEnd" class="my-auto font-extralight">End Time</Label>
+						<Input type="datetime-local" id="dateEnd" />
+					</div>
+				</div>
 			</div>
-		</div>
 
-		{@render configurationBox(CircleHelp, 'Question settings')}
-		{@render configurationBox(CircleCheck, 'Answer settings')}
-
-		<div>
-			{@render configurationBox(Info, 'Instructions')}
-		</div>
-
-		<!-- {#each configSettings as config}
-			<div class="my-2 flex w-full flex-row rounded-lg bg-white px-4 py-5 shadow">
-				<span class="bg-primary-foreground my-auto w-1/12 rounded-sm p-3">
-					<svelte:component this={config.icon} class="text-primary mx-auto" />
-				</span>
-				<p class="w-10/12 p-2 text-xl font-medium">{config.title}</p>
-				<span class="m-auto w-1/12"><ChevronDown class="mx-auto" /></span>
+			<div class="flex flex-row">
+				<div class="w-2/5">
+					{@render headingSubheading('Time limit', 'Set the maximum time allowed for the test')}
+				</div>
+				<div class=" flex w-3/5 flex-row gap-4">
+					<Input placeholder="Enter Time Limit in Minutes..." class="w-1/2" />
+				</div>
 			</div>
-		{/each} -->
+		</ConfigureBox>
+		<ConfigureBox title="Question settings" Icon={CircleHelp}>
+			<div class="flex flex-col gap-6">
+				{#snippet checkboxLabel(name: string, label: string, subLabel: string)}
+					<div class="flex w-2/5 items-center space-x-2">
+						<Checkbox id={name} />
+						<Label for={name} class="ml-2">
+							{@render headingSubheading(label, subLabel)}
+						</Label>
+					</div>
+				{/snippet}
+				{@render checkboxLabel('pointsCheckBox', 'Points', 'Display points for each question')}
+				{@render checkboxLabel('tagsCheckBox', 'Tags', 'Display the tags for each questions')}
+				{@render checkboxLabel(
+					'randomCheckBox',
+					'Randomize',
+					'Add random questions to test based on selected tags'
+				)}
+			</div>
+		</ConfigureBox>
 	</div>
 </div>

730bd2587872effe50369ed3ad7406e0a0023198
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-05-29 11:58:55 +0530

    schema for Test Management

---
+++ src/routes/(admin)/tests/test-template/schema.ts
@@ -0,0 +1,63 @@
+import { z } from 'zod';
+
+
+/*
+export const loginSchema = z.object({
+	username: z.string().email({ message: 'Invalid email address' }),
+	password: z.string().min(6, { message: 'Password must be at least 6 characters' })
+});
+*/
+export const testTemplateSchema = z.object({
+	name: z.string().min(3).max(100),
+	description: z.string().min(10).max(500),
+	start_time: z.string().min(10).max(100),
+	end_time: z.string().min(10).max(100),
+	time_limit: z.number().min(1).optional(),
+	marks_level: z.enum(['question', 'test']).optional(),
+	marks: z.number().min(0).optional(),
+	completion_message: z.string().min(10).max(500).optional(),
+	start_instructions: z.string().min(10).max(500).optional(),
+	link: z.string().url().optional(),
+	no_of_attempts: z.number().min(1).optional(),
+	shuffle: z.boolean().optional(),
+	random_questions: z.boolean().optional(),
+	no_of_random_questions: z.number().min(1).optional(),
+	question_pagination: z.number().min(1).optional(),
+	is_template: z.boolean().optional(),
+	template_id: z.number().min(1).optional(),
+	created_by_id: z.number().min(1).optional(),
+	tags: z.array(z.string()).default([]),
+	question_revision_ids: z.array(z.number()).default([]),
+	states: z.array(z.string()).default([])
+});
+
+export type FormSchema = typeof testTemplateSchema;
+
+
+/*
+
+	let testData = $state({
+		name: '',
+		description: '',
+		start_time: '',
+		end_time: '',
+		time_limit: 1,
+		marks_level: 'question',
+		marks: 0,
+		completion_message: '',
+		start_instructions: '',
+		link: '',
+		no_of_attempts: 1,
+		shuffle: false,
+		random_questions: false,
+		no_of_random_questions: 0,
+		question_pagination: 1,
+		is_template: false,
+		template_id: 0,
+		created_by_id: 0,
+		tags: [],
+		question_revision_ids: [],
+		states: []
+	});
+
+    */

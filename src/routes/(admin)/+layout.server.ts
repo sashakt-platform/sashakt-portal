37529e9fadcfc2179e9513262baad2fdd6abe546
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-05-31 17:45:41 +0530

    Get list of tags and states in layout server file

--- src/routes/(admin)/+layout.server.ts
+++ src/routes/(admin)/+layout.server.ts
@@ -1,9 +1,55 @@
 import { getRequestEvent } from '$app/server';
+import { BACKEND_URL } from '$env/static/private';
+import { getSessionTokenCookie } from '$lib/server/auth';
+import type { PageServerLoad } from '../$types';
+// export function load() {
+// 	const { locals } = getRequestEvent();
 
-export function load() {
+// 	return {
+// 		user: locals.user
+// 	};
+// }
+
+
+
+
+export const load: PageServerLoad = async () => {
+
 	const { locals } = getRequestEvent();
+	const token = getSessionTokenCookie();
+	const responseState = await fetch(`${BACKEND_URL}/api/v1/location/state/?skip=0&limit=100`, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	});
+
+	if (!responseState.ok) {
+		console.error('Failed to fetch states:', responseState.status, responseState.statusText);
+		return { states: null };
+	}
+
+	const states = await responseState.json();	
+
+
+	const responseTags = await fetch(`${BACKEND_URL}/api/v1/tag/?skip=0&limit=100`, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	});
+
+	if (!responseTags.ok) {
+		console.error('Failed to fetch states:', responseTags.status, responseTags.statusText);
+		return { states: null };
+	}
+
+	const tags = await responseTags.json();
 
 	return {
-		user: locals.user
+		// tests: tests,
+		user: locals.user,
+		states: states,
+		tags: tags
 	};
-}
+};

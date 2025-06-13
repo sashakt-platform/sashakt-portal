897bf6278820d8635f77f9677c4d1a48fa691946
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-06-13 12:25:52 +0530

    Single question Page Design

--- src/routes/(admin)/questionbank/single-question/+page.svelte
+++ src/routes/(admin)/questionbank/single-question/+page.svelte
@@ -8,15 +8,24 @@
 	import Plus from '@lucide/svelte/icons/plus';
 	import { Input } from '$lib/components/ui/input';
 	import Button from '$lib/components/ui/button/button.svelte';
+	import TagsSelection from '$lib/components/TagsSelection.svelte';
+	import { Switch } from '$lib/components/ui/switch/index.js';
+	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
+	import StateSelection from '$lib/components/StateSelection.svelte';
 
 	const { data } = $props();
+
+	let tags = $state<string[]>([]);
+	let states = $state<string[]>([]);
+
+	$effect(() => useSidebar().setOpen(false));
 
 	// This is a placeholder for the question bank data
 	// In a real application, you would fetch this data from an API or database
 </script>
 
-<div>
-	<div class="mx-10 flex flex-col gap-10 py-8 sm:w-[80%]">
+<div class="w-screen">
+	<div class="mx-auto flex flex-col gap-10 py-8 sm:w-[90%]">
 		{#snippet snippetHeading(title: string)}
 			<div class="bg-primary-foreground flex flex-row gap-3 rounded-sm px-4 py-3 align-middle">
 				<Label class="text-md my-auto font-bold">{title}</Label><Info
@@ -71,7 +80,7 @@
 									<Input class=" border-0" />
 								</div>
 								<div class="flex flex-row gap-2">
-									<Checkbox /><Label class="text-sm ">Set as mandatory</Label>
+									<Checkbox /><Label class="text-sm ">Set as correct answer</Label>
 								</div>
 							</div>
 						</div>
@@ -86,9 +95,27 @@
 					</div>
 				</div>
 			</div>
-			<div class="flex w-2/5 flex-col gap-2 pl-8">
-				{@render snippetHeading('Tags')}
+			<div class="flex w-2/5 flex-col pl-8">
+				<div class="flex h-1/2 flex-col gap-2">
+					{@render snippetHeading('Tags')}
+					<TagsSelection bind:tags />
+				</div>
+				<div class="flex h-1/2 flex-col gap-2">
+					{@render snippetHeading('States')}
+					<StateSelection bind:states />
+					<div class="mt-12 flex items-center space-x-2">
+						<Switch id="airplane-mode" class="bg-green-400" />
+						<Label for="airplane-mode">Active</Label><Info
+							class="my-auto w-4 align-middle text-xs text-gray-600"
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
+	</div>
+	<div class="sticky bottom-0 my-4 flex w-full justify-between border-t-4 bg-white p-4">
+		<Button variant="outline" class="text-primary border-primary border-1">Cancel</Button>
+
+		<Button class="bg-primary">Test</Button>
 	</div>
 </div>

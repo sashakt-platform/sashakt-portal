e4b8764d7f5d6afb30326131466299149f545783
Kurund Jalmi <kurund.jalmi@gmail.com>
2025-06-24 00:15:52 +0100

    dashboard cleanup

--- src/routes/(admin)/+layout.server.ts
+++ src/routes/(admin)/+layout.server.ts
@@ -3,10 +3,13 @@ import { BACKEND_URL } from '$env/static/private';
 import { getSessionTokenCookie } from '$lib/server/auth';
 import type { PageServerLoad } from '../$types';
 
-
 export const load: PageServerLoad = async () => {
 	const { locals } = getRequestEvent();
 	const token = getSessionTokenCookie();
+
+	let states = [];
+	let tags = [];
+
 	const responseState = await fetch(`${BACKEND_URL}/location/state/?skip=0&limit=100`, {
 		method: 'GET',
 		headers: {
@@ -16,10 +19,9 @@ export const load: PageServerLoad = async () => {
 
 	if (!responseState.ok) {
 		console.error('Failed to fetch states:', responseState.status, responseState.statusText);
-		return { states: null };
+	} else {
+		states = await responseState.json();
 	}
-
-	const states = await responseState.json();
 
 	const responseTags = await fetch(`${BACKEND_URL}/tag/?skip=0&limit=100`, {
 		method: 'GET',
@@ -30,67 +32,13 @@ export const load: PageServerLoad = async () => {
 
 	if (!responseTags.ok) {
 		console.error('Failed to fetch tags:', responseTags.status, responseTags.statusText);
-		return { states: null };
+	} else {
+		tags = await responseTags.json();
 	}
-
-	const tags = await responseTags.json();
-
-	const responseQuestions = await fetch(
-		`${BACKEND_URL}/questions/?skip=0&limit=100`,
-		{
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${token}`
-			}
-		}
-	);
-
-	if (!responseQuestions.ok) {
-		return { states: null };
-	}
-
-	const questions = await responseQuestions.json();
-
-
-	const responseUsers = await fetch(
-		`${BACKEND_URL}/users/`,
-		{
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${token}`
-			}
-		}
-	);
-
-	if (!responseUsers.ok) {
-		return { states: null };
-	}
-
-	const users = await responseUsers.json();
-
-
-	const responseTest = await fetch(
-		`${BACKEND_URL}/test/?is_template=0`,
-		{
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${token}`
-			}
-		}
-	);
-
-	if (!responseTest.ok) {
-		return { states: null };
-	}
-
-	const tests = await responseTest.json();
 
 	return {
-		tests: tests,
 		user: locals.user,
 		states: states,
-		tags: tags,
-		questions: questions,
-		users: users
+		tags: tags
 	};
 };

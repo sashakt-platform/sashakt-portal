6045c5f78c0510bab7e830237b1689894ea8903d
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-07-05 00:09:31 +0530

    Updating Question Listing

--- src/routes/(admin)/questionbank/+page.server.ts
+++ src/routes/(admin)/questionbank/+page.server.ts
@@ -5,35 +5,24 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async () => {
 	const token = getSessionTokenCookie();
 
-	const responseActive = await fetch(`${BACKEND_URL}/questions`, {
+	const response = await fetch(`${BACKEND_URL}/questions`, {
 		method: 'GET',
 		headers: {
 			Authorization: `Bearer ${token}`
 		}
 	});
 
-	if (!responseActive.ok) {
-		console.error('Failed to fetch questions:', responseActive.status, responseActive.statusText);
+	if (!response.ok) {
+		console.error('Failed to fetch questions:', response.status, response.statusText);
 		return { questions: null };
 	}
 
-	const questionsActive = await responseActive.json();
+	const questions = await response.json();
 
-	const responseInactive = await fetch(`${BACKEND_URL}/questions?is_active=false`, {
-		method: 'GET',
-		headers: {
-			Authorization: `Bearer ${token}`
-		}
-	});
 
-	if (!responseInactive.ok) {
-		console.error('Failed to fetch questions:', responseInactive.status, responseInactive.statusText);
-		return { questions: null };
-	}
 
-	const questionsInactive = await responseInactive.json();
 
 	return {
-		questions: questionsActive.concat(questionsInactive)
+		questions
 	};
 };

5c4545f7e76c9b19042890e86fd86b5e319bb4d5
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-07-05 13:40:39 +0530

    Creating custom DeleteDialog component

---
+++ src/lib/components/DeleteDialog.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+
+	let { action = $bindable(), elementName = 'Element' } = $props<{
+		action: string | null;
+		elementName?: string;
+	}>();
+
+	let open = $derived.by(() => (action ? true : false));
+</script>
+
+<AlertDialog.Root
+	bind:open
+	onOpenChange={(value) => {
+		if (!value) {
+			action = null; // Reset action when dialog is closed
+		}
+	}}
+>
+	<AlertDialog.Trigger class={buttonVariants({ variant: 'outline' })}>
+		Show Dialog
+	</AlertDialog.Trigger>
+	<AlertDialog.Content>
+		<AlertDialog.Header>
+			<AlertDialog.Title>Delete {elementName}?</AlertDialog.Title>
+			<AlertDialog.Description>
+				This action cannot be undone. This will permanently delete your {elementName}
+			</AlertDialog.Description>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<form {action} method="POST">
+				<AlertDialog.Cancel type="reset" onclick={() => (action = null)}>Cancel</AlertDialog.Cancel>
+
+				<AlertDialog.Action type="submit">Continue</AlertDialog.Action>
+			</form>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>

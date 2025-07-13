2407e73ebd178d3e50cd045250cc7a805ddaece6
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-07-13 17:30:03 +0530

    Making Tag-Type Non-optional

--- src/routes/(admin)/questionbank/single-question/[id]/schema.ts
+++ src/routes/(admin)/questionbank/single-question/[id]/schema.ts
@@ -1,37 +1,34 @@
 import { z } from 'zod';
 
-
 export const optionSchema = z.object({
-    id: z.number().int(),
-    key: z.string(),
-    value: z.string()
+	id: z.number().int(),
+	key: z.string(),
+	value: z.string()
 });
 
 export const questionSchema = z.object({
-        question_text: z.string().nonempty({ message: "Question text is required" }),
-        instructions: z.string().nullable().optional(),
-        question_type: z.enum(["single-choice", "multi-choice"]).default("single-choice"),
-        options: z.array(optionSchema).min(2).default([]),
-        correct_answer: z.array(z.number().int()).min(1).default([]),
-        subjective_answer_limit: z.number().int().positive().nullable().optional(),
-        is_mandatory: z.boolean().default(false),
-        solution: z.string().nullable().optional(),
-        organization_id: z.number().int().positive(),
-        state_ids: z.array(z.string()).default([]),
-        district_ids: z.array(z.string()).default([]),
-        block_ids: z.array(z.string()).default([]),
-        tag_ids: z.array(z.string()).default([]),
-        is_active: z.boolean().default(true),
+	question_text: z.string().nonempty({ message: 'Question text is required' }),
+	instructions: z.string().nullable().optional(),
+	question_type: z.enum(['single-choice', 'multi-choice']).default('single-choice'),
+	options: z.array(optionSchema).min(2).default([]),
+	correct_answer: z.array(z.number().int()).min(1).default([]),
+	subjective_answer_limit: z.number().int().positive().nullable().optional(),
+	is_mandatory: z.boolean().default(false),
+	solution: z.string().nullable().optional(),
+	organization_id: z.number().int().positive(),
+	state_ids: z.array(z.string()).default([]),
+	district_ids: z.array(z.string()).default([]),
+	block_ids: z.array(z.string()).default([]),
+	tag_ids: z.array(z.string()).default([]),
+	is_active: z.boolean().default(true)
 });
 
 export type FormSchema = typeof questionSchema;
 
-
-
 export const tagSchema = z.object({
-    description: z.string().nullable().optional(),
-    name: z.string().nonempty({ message: "Tag name is required" }),
-    tag_type_id: z.string(),
+	description: z.string().nullable().optional(),
+	name: z.string().nonempty({ message: 'Tag name is required' }),
+	tag_type_id: z.number().nullable().default(null).optional()
 });
 
 export type TagFormSchema = typeof tagSchema;

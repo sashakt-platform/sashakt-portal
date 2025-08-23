f5c7c05b1252ac4ff682eeb1102b96383cf32ffb
akkshitgupta <96991785+akkshitgupta@users.noreply.github.com>
2025-08-23 17:36:52 +0530

    fix: create labels for breadcrumbs

--- src/lib/breadcrumb.ts
+++ src/lib/breadcrumb.ts
@@ -7,6 +7,21 @@
  * ]
  */
 
+const routes: Record<string, string> = {
+	dashboard: 'Dashboard',
+	profile: 'Profile',
+	password: 'Update password',
+	questionbank: 'Question Bank',
+	'single-question': 'Create a question',
+	import: 'Add questions',
+	tags: 'Tag Management',
+	tests: 'Test Management',
+	users: 'User Management',
+	tag: 'Create a tag',
+	tagtype: 'Create a tag type',
+	add: 'Create new user'
+};
+
 export const breadcrumbs = (pathname: string) => {
 	const segments = pathname.split('/').slice(0, 3).filter(Boolean);
 
@@ -14,13 +29,18 @@ export const breadcrumbs = (pathname: string) => {
 	return segments.map((segment) => {
 		pathAccumulator += `/${segment}`;
 
-		// prettify the label ("order-history" -> "Order history")
-		const words = segment.replace(/[-_]+/g, ' ').split(' ');
-		const label = words
-			.map((w) => w.charAt(0).toUpperCase() + w.slice(1))
-			.join(' ')
-			// optional decode for dynamic IDs
-			.replace(/%20/g, ' ');
+		let label;
+		if (routes[segment]) {
+			label = routes[segment];
+		} else {
+			// prettify the label ("order-history" -> "Order history")
+			const words = segment.replace(/[-_]+/g, ' ').split(' ');
+			label = words
+				.map((w) => w.charAt(0).toUpperCase() + w.slice(1))
+				.join(' ')
+				// optional decode for dynamic IDs
+				.replace(/%20/g, ' ');
+		}
 
 		return { label, href: pathAccumulator };
 	});

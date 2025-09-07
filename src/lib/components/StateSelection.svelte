78a7d2dd31919ddaa4f638de18f2538d1c511e13
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-09-07 19:32:09 +0530

    Removing unused imports

--- src/lib/components/StateSelection.svelte
+++ src/lib/components/StateSelection.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
-	import Badge from '$lib/components/ui/badge/badge.svelte';
 	import { page } from '$app/state';
-	import * as Command from '$lib/components/ui/command/index.js';
-	import * as Popover from '$lib/components/ui/popover/index.js';
-	import { Button } from '$lib/components/ui/button/index.js';
-	import { cn } from '$lib/utils.js';
-	import CheckIcon from '@lucide/svelte/icons/check';
-	import ChevronsUpDownIcon from '@lucide/svelte/icons/chevrons-up-down';
-	import { goto } from '$app/navigation';
-	import { type Filter } from '$lib/types/filters';
 	import Filteration from './Filteration.svelte';
 
 	let stateList = $derived.by(() => page?.data?.states?.items ?? []);

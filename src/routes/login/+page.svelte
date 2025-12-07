d7205f10af1b5f5827257858e2ae6646e8c404af
Kurund Jalmi <kurund.jalmi@gmail.com>
2025-12-07 01:27:11 +0000

    minor fixes to login form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,8 +5,8 @@
 	let { data, event }: { data: PageData; event: Event } = $props();
 </script>
 
-<div class="flex h-screen items-center justify-center">
-	<Card.Root class="w-[350px]">
+<div class="flex h-screen items-center justify-center px-4">
+	<Card.Root class="w-full max-w-[350px]">
 		<Card.Header>
 			<Card.Title>Login to Sashakt</Card.Title>
 			<Card.Description>Please enter your email and password to login.</Card.Description>

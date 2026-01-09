4efe34f5d8f102653d442ca164343d946f71b578
Kurund Jalmi <kurund.jalmi@gmail.com>
2026-01-09 09:26:25 +0000

    Sentry config tweaks (#257)
    * disable sentry reporting for local instance
    
    * differentiate between staging and prod env for sentry
    
    * update cd workflows for staging and prod

--- src/instrumentation.server.ts
+++ src/instrumentation.server.ts
@@ -1,12 +1,17 @@
+import { dev } from '$app/environment';
+import { PUBLIC_APP_ENV } from '$env/static/public';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
 	dsn: 'https://91335032a1fef2b37caec0d0271a23a8@o412613.ingest.us.sentry.io/4510127619768320',
+	enabled: !dev,
 
 	tracesSampleRate: 1.0,
 
 	// Enable logs to be sent to Sentry
-	enableLogs: true
+	enableLogs: true,
+
+	environment: PUBLIC_APP_ENV || 'development'
 
 	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
 	// spotlight: import.meta.env.DEV,

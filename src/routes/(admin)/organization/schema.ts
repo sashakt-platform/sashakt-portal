0131a1af8053ce1e50b3efcb84b7495a99433fbe
Kurund Jalmi <kurund.jalmi@gmail.com>
2026-01-24 16:42:06 +0000

    update validation in edit mode

--- src/routes/(admin)/organization/schema.ts
+++ src/routes/(admin)/organization/schema.ts
@@ -5,13 +5,17 @@ export const editOrganizationSchema = z.object({
 	name: z.string().min(1, { message: 'Organization name is required' }),
 	shortcode: z.string().min(1, { message: 'Shortcode is required' }),
 	logo: z
-		.instanceof(File, { message: 'Please upload a valid image file' })
-		.refine((file) => file.size === 0 || file.type.startsWith('image/'), {
-			message: 'File must be an image'
-		})
-		.refine((file) => file.size === 0 || file.size <= 2 * 1024 * 1024, {
-			message: 'Image must be less than 2MB'
-		})
+		.union([
+			z
+				.instanceof(File, { message: 'Please upload a valid image file' })
+				.refine((file) => file.size === 0 || file.type.startsWith('image/'), {
+					message: 'File must be an image'
+				})
+				.refine((file) => file.size === 0 || file.size <= 2 * 1024 * 1024, {
+					message: 'Image must be less than 2MB'
+				}),
+			z.string()
+		])
 		.optional()
 });
 

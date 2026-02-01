3c9c656df41db9eda6559fcaf874409e6550bd7e
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2026-02-01 18:41:53 +0530

    Rendering Organization Details on Login Page (#287)
    * Rendering Organization Details on Login
    
    * Fixing existing test cases
    
    * saving organization-code to cookies for retrieval at logout
    
    * Optimization and mock support for test case
    
    * deletion of cookies when invalid org
    
    * Test cases for Org Public Data
    
    * Test cases for auth
    
    * UI Updation for logo
    
    * Renaming Cookie name
    
    ---------
    
    Co-authored-by: Kurund Jalmi <kurund.jalmi@gmail.com>

---
+++ src/routes/[organization]/+page.server.ts
@@ -0,0 +1,9 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const org = params.organization;
+
+	// encode to be safe
+	throw redirect(307, `/login?organization=${encodeURIComponent(org)}`);
+};

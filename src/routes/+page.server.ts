bcdd68e7ccbb9b15e9ff8342925716a82bb57bea
apeksha gangurde <apekshagangurde08@gmail.com>
2026-06-01 09:12:03 +0530

    Update redirect logic to route super admins to the organization page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,12 @@
 import { requireLogin } from '$lib/server/auth';
 import { redirect } from '@sveltejs/kit';
+import { isSuperAdmin } from '$lib/utils/permissions';
 
 export function load() {
 	const user = requireLogin();
 
 	if (user) {
-		redirect(307, '/tests/test-session');
+		redirect(307, isSuperAdmin(user) ? '/organization' : '/tests/test-session');
 	} else {
 		redirect(307, '/login');
 	}

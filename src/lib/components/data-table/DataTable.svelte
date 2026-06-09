7d21d93d12a690858804182869d404137a448e69
Ninad Khanolkar <ninad1701@gmail.com>
2026-06-09 15:52:59 +0530

    Updating the style for overflow property

--- src/lib/components/data-table/DataTable.svelte
+++ src/lib/components/data-table/DataTable.svelte
@@ -192,8 +192,7 @@
 </script>
 
 <div>
-	<div class="overflow-x-auto rounded-2xl border">
-		<Table.Root>
+	<Table.Root>
 			<Table.Header>
 				{#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
 					<Table.Row class="bg-primary-foreground">
@@ -265,7 +264,6 @@
 				{/each}
 			</Table.Body>
 		</Table.Root>
-	</div>
 	<div class="flex flex-col items-center gap-3 py-4 sm:flex-row sm:justify-between sm:gap-2">
 		<div class="text-muted-foreground text-center text-sm sm:flex-1 sm:text-left">
 			{#if enableSelection}

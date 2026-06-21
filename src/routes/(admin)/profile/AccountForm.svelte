4277c6d8f34e5a54827be9a16d637caa56a085bd
CodeSrushti-3105 <158837536+CodeSrushti-3105@users.noreply.github.com>
2026-06-21 16:28:07 +0530

    fix: conditionally render state field based on user role

--- src/routes/(admin)/profile/AccountForm.svelte
+++ src/routes/(admin)/profile/AccountForm.svelte
@@ -63,17 +63,19 @@
 						disabled
 					/>
 				</div>
-				<div class="flex flex-col gap-1.5">
-					<label for="state-field" class="text-sm font-medium">
-						State
-					</label>
-					<Input
-						id="state-field"
-						class="disabled:bg-background"
-						value={$formData.state_name ?? ''}
-						disabled
-					/>
-				</div>
+				{#if $formData.role_label === 'State Admin'}
+					<div class="flex flex-col gap-1.5">
+						<label for="state-field" class="text-sm font-medium">
+							State
+						</label>
+						<Input
+							id="state-field"
+							class="disabled:bg-background"
+							value={$formData.state_name ?? ''}
+							disabled
+						/>
+					</div>
+				{/if}
 			</div>
 		</div>
 	</div>
